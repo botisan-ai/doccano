ae86352f7e6b4e54dd8e13cb9d7b7c9037f0bf0c
Clovin <wxxzhuang@gmail.com>
2023-01-04 16:28:35 +0800

    fix: okta login param

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -284,6 +284,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 SOCIALACCOUNT_PROVIDERS = {
     "okta": {
+        "OKTA_CALLBACK_URL": env("OAUTH_OKTA_OAUTH2_CALLBACK_URL", ""),
         "OKTA_BASE_URL": env("OAUTH_OKTA_OAUTH2_API_URL", ""),
         "OAUTH_PKCE_ENABLED": True,
         "APP": {"client_id": env("OAUTH_OKTA_OAUTH2_KEY", ""), "secret": env("OAUTH_OKTA_OAUTH2_SECRET", "")},
--- backend/social/okta.py
+++ backend/social/okta.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.providers.okta.views import OktaOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
@@ -5,5 +6,5 @@ from dj_rest_auth.registration.views import SocialLoginView
 
 class OktaLogin(SocialLoginView):
     adapter_class = OktaOAuth2Adapter
-    callback_url = "/projects"
+    callback_url = settings.SOCIALACCOUNT_PROVIDERS.get("okta").get("OKTA_CALLBACK_URL")
     client_class = OAuth2Client
