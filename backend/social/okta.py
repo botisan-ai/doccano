c45958da85c8c403dda0429592a334133239dc75
Clovin <wxxzhuang@gmail.com>
2023-01-11 15:05:48 +0800

    fix: redirect issue

--- backend/social/okta.py
+++ backend/social/okta.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.http import HttpResponseRedirect
 from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 from allauth.socialaccount.providers.okta.views import OktaOAuth2Adapter
 from dj_rest_auth.registration.views import SocialLoginView
@@ -8,3 +9,11 @@ class OktaLogin(SocialLoginView):
     adapter_class = OktaOAuth2Adapter
     callback_url = settings.SOCIALACCOUNT_PROVIDERS.get("okta").get("OKTA_CALLBACK_URL")
     client_class = OAuth2Client
+
+    def post(self, request, *args, **kwargs):
+        self.request = request
+        self.serializer = self.get_serializer(data=self.request.data)
+        self.serializer.is_valid(raise_exception=True)
+
+        self.login()
+        return HttpResponseRedirect(settings.SOCIALACCOUNT_PROVIDERS.get("okta").get("OKTA_CALLBACK_URL").replace('/social/complete/okta-oauth2/', '/projects'))
